74259b4967386cefea479ad487c26af7b1c46fe8
Talon Meyer <talonmeyer12@gmail.com>
2023-04-26 01:23:36 -0700

    Updated user route, less lines

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,7 @@ module.exports = {
     async getUsers(req, res) {
         try {
             const users = await User.find();
-            const usersObj = {
-                users
-            }
-            return res.json(usersObj);
+            return res.json(users);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);

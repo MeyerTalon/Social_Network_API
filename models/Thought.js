ae3753d6669e8feea48c1f4a18ade841e0c3320f
Talon Meyer <talonmeyer12@gmail.com>
2023-04-25 21:05:54 -0700

    Created models and schemas, need to be tested

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date)=> {
+            return `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}`
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: "User"
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reaction.length;
+  });
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
